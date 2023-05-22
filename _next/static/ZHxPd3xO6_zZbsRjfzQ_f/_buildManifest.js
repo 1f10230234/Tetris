2313f218cbc08da66ad9bcafb13116eb4b97d4e3
1f10230234 <1f10230234@users.noreply.github.com>
2023-05-22 09:01:06 +0000

    deploy: 3c710b955db119b3bbab06111834d32a80980af8

---
+++ _next/static/ZHxPd3xO6_zZbsRjfzQ_f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/db3ecc21f3d4ee68.css","static/chunks/pages/index-cca1eac4ce606a18.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
