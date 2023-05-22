d92bc2d3759ea056c4805e3dfe6a012751380f61
1f10230234 <1f10230234@users.noreply.github.com>
2023-05-22 09:15:52 +0000

    deploy: 29ccba9f3ff886895156d88db777a2a195cc7265

---
+++ _next/static/wz0QkF6rW_WtoK_oz-FTx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/db3ecc21f3d4ee68.css","static/chunks/pages/index-f5dbff4709cb5e9f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
