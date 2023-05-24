e8677be9da6de26c66da0e70d6651f6bb2b3ad5b
1f10230234 <s1f102302348@iniad.org>
2023-05-24 15:58:34 +0900

    キーボードと左右制御

--- src/Hooks/useBoard.ts
+++ src/Hooks/useBoard.ts
@@ -1,3 +1,3 @@
-import { useEffect, useMemo, useState } from 'react';
-import { createArray } from '../utils/createArray';
-import { mino } from '../utils/mino';
+// import { useEffect, useMemo, useState } from 'react';
+// import { createArray } from '../utils/createArray';
+// import { mino } from '../utils/mino';
